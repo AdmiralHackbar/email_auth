a8549e182499ffc2cd69106473236af6af633131
Daniel Gephardt <the.admiral.hackbar@gmail.com>
2012-11-30 21:40:30 -0600

    Updating the AuthUser model and adding the AuthToken model

--- email_auth_app/models.py
+++ email_auth_app/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
-# Create your models here.
 class AuthUser(models.Model):
-    email = models.CharField(max_length=200)
+    email = models.CharField(max_length=300, primary_key=True)
+
+class AuthToken(models.Model):
+    authUser = models.ForeignKey(AuthUser)
+    token = models.CharField(max_length=50, primary_key=True)
+    timestamp = models.TimeField(auto_now_add=True)
