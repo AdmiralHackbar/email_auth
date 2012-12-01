281e123c456201cc6f63cb6f113eb8ae850cafc0
Daniel Gephardt <the.admiral.hackbar@gmail.com>
2012-11-30 21:52:37 -0600

    Adding the field 'name' to AuthUser

--- email_auth_app/models.py
+++ email_auth_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class AuthUser(models.Model):
     email = models.CharField(max_length=300, primary_key=True)
+    name = models.CharField(max_length=200)
 
 class AuthToken(models.Model):
     authUser = models.ForeignKey(AuthUser)
